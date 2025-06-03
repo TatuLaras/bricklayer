a0ad91b31a81ce8c131817952e8381ca378913c9
Tatu Laras <tatu.laras@gmail.com>
2025-06-03 21:13:22 +0300

    Updated to use firewatch.h for listening for file changes

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
+#define FIREWATCH_IMPLEMENTATION
+#include "firewatch.h"
+
 #include "aseprite_texture.h"
-#include "model_vector.h"
 #include "orbital_controls.h"
 #include "path.h"
 #include "raylib.h"
@@ -39,84 +41,98 @@ static const char *vertex_shader =
     "    gl_Position = mvp*vec4(vertexPosition, 1.0); \n"
     "}                                  \n";
 
-// Tries to load a texture from aseprite file at `filepath` and applies it
-// to `model` mesh at index 0 on success.
-static void try_load_corresponding_texture(const char *filepath, Model *model) {
-    Image *image = aseprite_load_as_image(filepath);
-    if (!image)
-        return;
+static Shader shader = {0};
+static Model *models = 0;
+static pthread_mutex_t lock;
+static size_t model_count = 0;
 
-    Texture2D texture = LoadTextureFromImage(*image);
-    UnloadImage(*image);
-    free(image);
-    if (!texture.id)
-        return;
+typedef enum {
+    FILE_KIND_MODEL,
+    FILE_KIND_TEXTURE,
+} FileKind;
 
-    model->materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
+void load_model(const char *filepath, uint64_t model_index) {
+    pthread_mutex_lock(&lock);
+
+    Texture texture = {0};
+    if (models[model_index].materialCount)
+        texture =
+            models[model_index].materials[0].maps[MATERIAL_MAP_DIFFUSE].texture;
+
+    if (models[model_index].meshCount)
+        UnloadModel(models[model_index]);
+
+    models[model_index] = LoadModel(filepath);
+    assert(models[model_index].meshCount);
+    models[model_index].materials[0].shader = shader;
+    models[model_index].materials[0].maps[MATERIAL_MAP_DIFFUSE].texture =
+        texture;
+
+    pthread_mutex_unlock(&lock);
 }
 
-// Loads model data from `model_filepaths` supplied and possible aseprite
-// texture files of the same name.
-static ModelVector load_model_data_from_files(StringVector *model_filepaths,
-                                              Shader *shader) {
-    ModelVector vec = modelvec_init();
+void load_texture(const char *filepath, uint64_t model_index) {
+    pthread_mutex_lock(&lock);
 
-    size_t i = 0;
-    while (1) {
-        char *model_filepath = stringvec_get(model_filepaths, i++);
-        if (!model_filepath)
-            break;
-        ModelData model_data = {.model = LoadModel(model_filepath)};
-
-        char *texture_filepath =
-            path_get_corresponding_texture_file(model_filepath);
-        try_load_corresponding_texture(texture_filepath, &model_data.model);
-        free(texture_filepath);
-
-        modelvec_append(&vec, model_data);
-        model_data.model.materials[0].shader = *shader;
+    ImageData image_data = aseprite_load(filepath);
+    if (!image_data.base_image.data) {
+        pthread_mutex_unlock(&lock);
+        return;
     }
-    return vec;
+
+    Texture2D texture = LoadTextureFromImage(image_data.base_image);
+    UnloadImage(image_data.base_image);
+    assert(texture.id);
+
+    if (models[model_index].materialCount) {
+        if (models[model_index]
+                .materials[0]
+                .maps[MATERIAL_MAP_DIFFUSE]
+                .texture.id)
+            UnloadTexture(models[model_index]
+                              .materials[0]
+                              .maps[MATERIAL_MAP_DIFFUSE]
+                              .texture);
+
+        models[model_index].materials[0].maps[MATERIAL_MAP_DIFFUSE].texture =
+            texture;
+    }
+
+    pthread_mutex_unlock(&lock);
 }
 
-static inline uint64_t file_last_modified(const char *filepath) {
-    struct stat attr;
-    if (stat(filepath, &attr))
-        return 0;
-    return attr.st_mtim.tv_sec;
-}
+static inline void setup_models(StringVector *model_filepaths) {
+    model_count = model_filepaths->indices_used;
+    assert(model_count);
+    models = calloc(model_count, sizeof(Model));
+    assert(models);
 
-static inline uint64_t max(uint64_t a, uint64_t b) {
-    if (a > b)
-        return a;
-    return b;
-}
-
-static uint64_t
-get_most_recent_file_modification(StringVector *model_filepaths) {
-    size_t i = 0;
-    uint64_t last_modification = 0;
-
-    while (1) {
-        char *model_filepath = stringvec_get(model_filepaths, i++);
+    for (size_t i = 0; i < model_count; i++) {
+        char *model_filepath = stringvec_get(model_filepaths, i);
         if (!model_filepath)
             break;
 
+        firewatch_new_file_ex(model_filepath, 0, i, FILE_KIND_MODEL);
+        load_model(model_filepath, i);
+
         char *texture_filepath =
             path_get_corresponding_texture_file(model_filepath);
+        assert(texture_filepath);
+        firewatch_new_file_ex(texture_filepath, 0, i, FILE_KIND_TEXTURE);
+        load_texture(texture_filepath, i);
+        free(texture_filepath);
+    }
+}
 
-        last_modification =
-            max(last_modification, file_last_modified(model_filepath));
-
-        if (!texture_filepath)
+static inline void unload_models(void) {
+    for (size_t i = 0; i < model_count; i++) {
+        if (!models[i].meshCount)
             continue;
-        last_modification =
-            max(last_modification, file_last_modified(texture_filepath));
-
-        free(texture_filepath);
+        if (models[i].materials[0].maps[MATERIAL_MAP_DIFFUSE].texture.id)
+            UnloadTexture(
+                models[i].materials[0].maps[MATERIAL_MAP_DIFFUSE].texture);
+        UnloadModel(models[i]);
     }
-
-    return last_modification;
 }
 
 int main(int argc, char **argv) {
@@ -146,10 +162,8 @@ int main(int argc, char **argv) {
     InitWindow(800, 450, "Bricklayer");
     SetTargetFPS(60);
 
-    Shader shader = LoadShaderFromMemory(vertex_shader, 0);
-
-    uint64_t last_modified =
-        get_most_recent_file_modification(&model_filepaths);
+    shader = LoadShaderFromMemory(vertex_shader, 0);
+    setup_models(&model_filepaths);
 
     Camera starting_camera = {
         .position = (Vector3){0.0f, 1.0f, 3.0f},
@@ -159,26 +173,22 @@ int main(int argc, char **argv) {
     };
     Camera camera = starting_camera;
 
-    ModelVector models = load_model_data_from_files(&model_filepaths, &shader);
+    LoadRequest load_request = {0};
 
     int wireframe_enabled = 0;
-    float time_since_last_modified_check = 0;
 
     while (!WindowShouldClose()) {
-        time_since_last_modified_check += GetFrameTime();
-
-        // Modified check if needed
-        if (time_since_last_modified_check > MODIFIED_CHECK_COOLDOWN_SECONDS) {
-            time_since_last_modified_check = 0;
-
-            uint64_t new_last_modified =
-                get_most_recent_file_modification(&model_filepaths);
-
-            // Reload model if needed
-            if (new_last_modified > last_modified) {
-                modelvec_free(&models);
-                models = load_model_data_from_files(&model_filepaths, &shader);
-                last_modified = new_last_modified;
+        // Check for file changes
+        while (firewatch_request_stack_pop(&load_request)) {
+            switch (load_request.kind) {
+            case FILE_KIND_MODEL:
+                load_model(load_request.filepath, load_request.cookie);
+                break;
+            case FILE_KIND_TEXTURE:
+                load_texture(load_request.filepath, load_request.cookie);
+                break;
+            default:
+                break;
             }
         }
 
@@ -187,7 +197,7 @@ int main(int argc, char **argv) {
         if (IsMouseButtonReleased(MOUSE_BUTTON_MIDDLE))
             EnableCursor();
         if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE)) {
-            orbital_camera_update(&camera);
+            orbital_camera_update(&camera, 0);
         }
         orbital_adjust_camera_zoom(&camera, GetMouseWheelMove());
 
@@ -209,14 +219,12 @@ int main(int argc, char **argv) {
 
         BeginMode3D(camera);
 
-        size_t i = 0;
-        while (1) {
-            ModelData *model = modelvec_get(&models, i++);
-            if (!model)
-                break;
-            DrawModel(model->model, Vector3Zero(), 1.0f, RAYWHITE);
+        for (size_t i = 0; i < model_count; i++) {
+            assert(models[i].meshCount);
+
+            DrawModel(models[i], Vector3Zero(), 1.0f, RAYWHITE);
             if (wireframe_enabled)
-                DrawModelWires(model->model, Vector3Zero(), 1.0f, BLACK);
+                DrawModelWires(models[i], Vector3Zero(), 1.0f, BLACK);
         }
 
         if (grid_enabled)
@@ -226,12 +234,7 @@ int main(int argc, char **argv) {
         EndDrawing();
     }
 
-    size_t i = 0;
-    ModelData *model = 0;
-    while ((model = modelvec_get(&models, i++)))
-        UnloadModel(model->model);
-
-    modelvec_free(&models);
+    unload_models();
     stringvec_free(&model_filepaths);
     UnloadShader(shader);
     CloseWindow();
--- src/path.h
+++ src/path.h
@@ -1,7 +1,7 @@
 #ifndef _PATH
 #define _PATH
 
-// Replaces the last three characters of a string with "png". Free returned
+// Replaces the last three characters of a string with "aseprite". Free returned
 // char* after use.
 char *path_get_corresponding_texture_file(const char *src);
 
